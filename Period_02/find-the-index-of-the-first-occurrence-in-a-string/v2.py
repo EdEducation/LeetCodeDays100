f0c358a54bb545af64a769539fd0834c2c5f2715
Andrew Evsugin <ondrya@yandex.ru>
2026-04-30 23:49:06 +0300

    fin task

--- Period_02/find-the-index-of-the-first-occurrence-in-a-string/v2.py
+++ Period_02/find-the-index-of-the-first-occurrence-in-a-string/v2.py
@@ -40,7 +40,23 @@ class Solution:
         Returns:
             int: индекс первого вхождения или -1
         """
-        return haystack.find(needle)
+        if not needle:
+            return 0
+        
+        n = len(haystack)
+        m = len(needle)
+        
+        # Если needle длиннее haystack, сразу возвращаем -1
+        if m > n:
+            return -1
+        
+        # Пробегаем по всем возможным позициям начала
+        for i in range(n - m + 1):
+            # Проверяем совпадение подстроки
+            if haystack[i:i + m] == needle:
+                return i
+        
+        return -1
 
 
 # Тесты
